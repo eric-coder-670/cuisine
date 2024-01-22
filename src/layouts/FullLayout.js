8eb2e625c90d3ac2f19f9614c33cf4f7ef5b7a05
eric <ericfridolin85@gmail.com>
2024-01-22 14:20:19 +0100

    update

--- src/layouts/FullLayout.js
+++ src/layouts/FullLayout.js
@@ -3,36 +3,43 @@ import Sidebar from "./Sidebar";
 import Header from "./Header";
 import { Container } from "reactstrap";
 import { useEffect, useState } from "react";
-import axios from "axios"
-
+import axios from "axios";
 
 const FullLayout = () => {
-  const [category, setaCategory] = useState([]);
+  const [category, setCategory] = useState([]);
 
-  const fetchCtaegory = async () => {
-
-    const response = await axios.post(`http://localhost:5000/list/category`);
-    const  {categories}  = response.data;
-    console.log(categories);
-    setaCategory(categories);
-  }
+  const fetchCategory = async () => {
+    try {
+      const response = await axios.post(`http://localhost:5000/list/category`);
+      const { categories } = response.data;
+      setCategory(categories);
+    } catch (error) {
+      console.error("Error fetching categories:", error.message);
+    }
+  };
 
   useEffect(() => {
-    fetchCtaegory();
+    fetchCategory();
   }, []);
 
-
   return (
-    <main>
-      {/********header**********/}
+    <main style={{ display: "flex", flexDirection: "column", minHeight: "100vh" }}>
+      {/********Header**********/}
       <Header listCategory={category} />
-      <div className="pageWrapper d-lg-flex">
+      <div
+        className="pageWrapper d-lg-flex"
+        style={{ display: "flex", flexGrow: 1 }}
+      >
         {/********Sidebar**********/}
-        <aside className="sidebarArea shadow" id="sidebarArea" >
+        <aside
+          className="sidebarArea shadow"
+          id="sidebarArea"
+          style={{overflowY: "auto" }}
+        >
           <Sidebar />
         </aside>
         {/********Content Area**********/}
-        <div className="contentArea">
+        <div className="contentArea" style={{ flex: "1", overflowY: "auto" }}>
           {/********Middle Content**********/}
           <Container className="p-4" fluid>
             <Outlet />
